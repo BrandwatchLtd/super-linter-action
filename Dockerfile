eaadd3cbf1c62317ad417a42ce82bf72cc62c66c
Ewen Cluley <ewen@brandwatch.com>
2021-02-19 13:27:42 +0000

    add reviewdog

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM github/super-linter
 COPY rules/ /rules/
 COPY entrypoint.sh /entrypoint.sh
 
+ENV REVIEWDOG_VERSION=v0.10.0
+ENV INPUT_CHECKSTYLE_VERSION=8.37
+
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
+
 # We only want VALIDATE_DOCKERFILE_HADOLINT, which is enabled by default
 ENV VALIDATE_DOCKERFILE=false
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,16 @@ if [[ ${RUN_LOCAL} != "false" ]]; then
 	GITHUB_WORKSPACE="/tmp/lint/"
 fi
 
+export OUTPUT_DETAILS=detailed
+export OUTPUT_FORMAT=tap
+export DISABLE_ERRORS=true
+mkdir super-linter.report 
+
 echo "Workspace path: $GITHUB_WORKSPACE"
 RELATIVE_RULES_PATH=$(realpath --relative-to=${GITHUB_WORKSPACE} /rules/)
 echo "Rules path relative to workspace: $RELATIVE_RULES_PATH"
 export LINTER_RULES_PATH=$RELATIVE_RULES_PATH
 /action/lib/linter.sh
+echo "Report dir contents"
+ls super-linter.report
+cat super-linter.report/super-linter-JAVA.tap
